b2e6ccd1e2932a4e40f05d38eb303bfc2f69b9e4
Raphaël Ferrand <ferrand@protonmail.ch>
2018-06-26 18:02:59 +0200

    FIX: makes homescreen responsive

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZ29sZGVucmFwaHRpIiwiYSI6ImNqZDVzaGQ3dzFsYnAycW9xN25oa2VodmgifQ.AO002okeRrHyFgAhph-VMA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
